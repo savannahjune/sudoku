28cc0b8804c7ba835baaf4a93cbe34a1743aac5b
Savannah Henderson <savannahjune@gmail.com>
2015-01-08 23:54:24 -0800

    made functions that reate rows, columns, and nine squares from the matrix to see if they conform to rules of sudoku using the checkSeries function

--- static/js/sudoku_checker.js
+++ static/js/sudoku_checker.js
@@ -1,20 +1,10 @@
-// matrix = [ [1,2,4...], [2,4,6,...], [7,8,9,...] ]
-// col = [1, 2, 3]
 
-// COL_TO_CAPTURE = 2
-
-
-// for row in matrix:
-//	col.append(row[COL_TO_CAPTURE])
 
 $(document).ready(function () {
 
 	$("#check-submission").click(function(evt) {
 		evt.preventDefault();
 		console.log( "Pressed the button y'all" );
-		// iterate over rows
-			// iterate over colors
-				// matrix[row][col] = $(' + String(row) + '-' + String(col) + ').val()
 
 		var answerMatrix = [];
 
@@ -36,10 +26,45 @@ $(document).ready(function () {
 			answerMatrix.push(arrayRow);
 		}
 		console.log(answerMatrix);
-		// Figure out how to feed every row / col
+		
+		checkRows(answerMatrix);
+		checkColumns(answerMatrix);
 
 	});
 });
+
+function checkRows(answerMatrix) {
+	for (var row=0; row<9; row++) {
+		checkSeries(answerMatrix[row]);
+	}
+}
+
+function checkColumns(answerMatrix) {
+	var columnArray = [];
+	for (var column = 0; column < 9; column++) {
+		for (var row = 0; row < 9; row++) {
+			columnArray.push(answerMatrix[row][column]);
+		}
+		checkSeries(columnArray);
+	}
+}
+
+function checkSquares(answerMatrix) {
+	var col_offset = [0, 3, 6];
+	var row_offset = [0, 3, 6];
+
+	for (var square_column_index = 0; square_column_index < 3; square_column_index++) {
+		for (var square_row_index = 0; square_row_index < 3; square_row_index++) {
+			var squareArray = [];
+			for (var column = col_offset[square_column_index]; column < col_offset[square_column_index] + 3; column++) {
+				for (var row = row_offset[square_row_index]; row < row_offset[square_row_index] + 3; row++) {
+					squareArray.push(answerMatrix[row][column]);
+				}
+			}
+			checkSeries(squareArray);
+		}
+	}
+}
 
 /** 
 * Checks an array to make sure it is unique numbers from 1-9
@@ -52,7 +77,6 @@ function checkSeries(sudokuArray) {
 	}
 
 	var sortedArray = sudokuArray.sort(sortNumber);
-
 	var numPrevious = sortedArray[0];
 
 	if (numPrevious !== 1 || sortedArray.length !== 9) {
@@ -63,7 +87,6 @@ function checkSeries(sudokuArray) {
 	for (var i = 1; i < sortedArray.length - 1; i++) {
 		var number = sortedArray[i];
 		console.log('testing that ' + number + ' != ' + numPrevious);
-
 		if (numPrevious === number) {
 			console.log('Array is invalid!');
 			return false;
@@ -73,6 +96,8 @@ function checkSeries(sudokuArray) {
 	console.log('Array is valid!');
 	return true;
 }
+
+
 // perfect array
 // var numArray = [6, 4, 2, 3, 9, 7, 1, 8, 5];
 // array with duplicates
@@ -80,7 +105,9 @@ function checkSeries(sudokuArray) {
 // array with not enough answers
 // var numArray = [3, 2, 7, 1, 5, 8, 9, 4];
 // array with no one as first entry
-var numArray = [3, 2, 7, 5, 8, 9, 4, 8, 7];
+// var numArray = [3, 2, 7, 5, 8, 9, 4, 8, 7];
 
-checkSeries(numArray);
+// checkSeries(numArray);
+
+
 
